d75a0d161762ec936a65e9bcba23ae6fce68d4c3
pedrolima2106 <pedrolima2106@gmail.com>
2023-10-29 21:45:32 -0300

    tentando criar o banco

---
+++ Folha01/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Folha01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Folha01.Data
+{
+    public class Context : DbContext 
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<ListaModel> Lista { get; set; }
+    }
+}
---
+++ Folha01/Models/ListaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Folha01.Models
+{
+    public class ListaModel
+    {
+
+        public int IdFuncionario { get; set; }
+        public string NameFuncionario { get; set; }
+        public double Cpf { get; set; }
+        public double Cep { get; set; }
+        public DateTime DatadeNascimento { get; set; }
+        public string Gmail { get; set; }
+    }
+}
