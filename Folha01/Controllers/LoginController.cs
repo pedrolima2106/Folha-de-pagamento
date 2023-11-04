40dd5c0a55d77f1c1d493a98fab10fad78e11ebd
pedrolima2106 <pedrolima2106@gmail.com>
2023-11-03 22:38:28 -0300

    Tela de login parte1

---
+++ Folha01/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Folha01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace Folha01.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(loginModel.login == "adm" && loginModel.senha == "123")
+                    {
+                        return RedirectToAction("Index","Home");
+                    }
+                    TempData["MensagemErro"] = $"Usuario ou Senha Invalidos:";
+                }
+
+                return View("Index");
+
+            }
+            catch (Exception erro) 
+            {
+                TempData["MensagemErro"] = $"Não foi possivel Realizar o Login: {erro.Message}";
+                return RedirectToAction("Index");
+
+            }
+        }
+    }
+}
---
+++ Folha01/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Folha01.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite o Login")]
+        public string login { get; set; }
+
+        [Required(ErrorMessage = "Digite a Senha")]
+        public string senha { get; set; }
+
+    }
+}
